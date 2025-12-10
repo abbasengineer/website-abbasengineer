1f7256ca2398b149e5c841d3e03a4a96a7dc84dc
Abbas Engineer <abbster52@hotmail.com>
2025-12-09 21:24:51 -0800

    Add files via upload

---
+++ Resume-Page-Builder/client/src/App.tsx
@@ -0,0 +1,34 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import NotFound from "@/pages/not-found";
+import Layout from "@/components/Layout";
+import Resume from "@/pages/Resume";
+import CloudflareFit from "@/pages/CloudflareFit";
+
+function Router() {
+  return (
+    <Layout>
+      <Switch>
+        <Route path="/" component={Resume} />
+        <Route path="/cloudflare-fit" component={CloudflareFit} />
+        <Route component={NotFound} />
+      </Switch>
+    </Layout>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Router />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
